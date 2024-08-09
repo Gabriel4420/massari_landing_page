b6365eb62d3931829de994873717a56df60a7aa8
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-08-09 19:42:33 -0300

    feat: :sparkles: home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,29 @@
+import Hero from "@/components/hero";
+import Header from "../../components/header";
+import Head from "next/head";
+import OurServices from "@/components/ourServices";
+import About1 from "@/components/aboutUs";
+import SignalsPanel from "@/components/signalsPanel";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Massari Systems</title>
+        <meta
+          name="description"
+          content="Financial solutions for your business"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="min-h-screen ">
+        <Hero />
+        {/* Adicionar outros componentes conforme necessário */}
+        <OurServices />
+        <About1 />
+        <SignalsPanel/>
+      </main>
+    </div>
+  );
+}
