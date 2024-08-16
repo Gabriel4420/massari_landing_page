9c26528e9d133c44809cea3fffc771139b1cfa0b
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-08-16 11:14:53 +0000

    feat: :sparkles: alter email

--- src/components/views/contactUs.tsx
+++ src/components/views/contactUs.tsx
@@ -1,6 +1,6 @@
-'use client';
-import React from 'react';
-import { motion } from 'framer-motion';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
 
 const ContactUs: React.FC = () => {
   return (
@@ -8,20 +8,25 @@ const ContactUs: React.FC = () => {
       className="bg-white text-gray-800 py-24 px-8 md:px-16 rounded-lg shadow-lg"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: 'easeOut' }}
-      id='contact'
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      id="contact"
     >
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between">
         {/* Texto e Título */}
         <div className="md:w-1/2 mb-8 md:mb-0">
-          <h2 className="text-4xl font-bold mb-6">
-            Get in Touch
-          </h2>
+          <h2 className="text-4xl font-bold mb-6">Get in Touch</h2>
           <p className="text-lg mb-6">
-            Have questions or need help? Contact us via the form or through our support channels. We&apos;re here to help you!
+            Have questions or need help? Contact us via the form or through our
+            support channels. We&apos;re here to help you!
           </p>
           <p className="text-lg">
-            Email us at <a href="mailto:contact@massarisystems.com" className="text-[#ffc501] underline">contact@massarisystems.com</a>
+            Email us at{" "}
+            <a
+              href="mailto:rafael.azevedo@massarisystems.com"
+              className="text-[#ffc501] underline"
+            >
+              rafael.azevedo@massarisystems.com
+            </a>
           </p>
         </div>
 
@@ -29,7 +34,9 @@ const ContactUs: React.FC = () => {
         <div className="md:w-1/2 bg-white p-8 rounded-lg shadow-lg text-[#ffc501]">
           <form action="#">
             <div className="mb-4">
-              <label htmlFor="name" className="block text-sm font-medium mb-2">Name</label>
+              <label htmlFor="name" className="block text-sm font-medium mb-2">
+                Name
+              </label>
               <input
                 type="text"
                 id="name"
@@ -39,7 +46,9 @@ const ContactUs: React.FC = () => {
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="email" className="block text-sm font-medium mb-2">Email</label>
+              <label htmlFor="email" className="block text-sm font-medium mb-2">
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
@@ -49,7 +58,12 @@ const ContactUs: React.FC = () => {
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="message" className="block text-sm font-medium mb-2">Message</label>
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium mb-2"
+              >
+                Message
+              </label>
               <textarea
                 id="message"
                 name="message"
