57dfb30d384a460dd3d89333e05514a6c185a78d
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-08-16 07:17:24 -0300

    fix: :bug: fixing screen overflow issue

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           content="Financial solutions for your business"
         />
         <meta name="theme-color" content="#ffc501" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
