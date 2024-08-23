4b5fc2eb63316f685428c8e4eb13f37c29617ef8
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-08-23 12:11:14 +0000

    fix: :bug: fixing button accordion oportunities

--- src/components/views/accordionJobOpportunities.tsx
+++ src/components/views/accordionJobOpportunities.tsx
@@ -76,12 +76,12 @@ const AccordionJobOpportunities: React.FC<JobOpportunitiesProps> = ({
                 >
                   <div className="p-4 bg-[#cc9d00] rounded-b-lg">
                     <p className="mb-4">{opportunity.description}</p>
-                    <button
-                      onClick={() => openModal(opportunity)}
+                    <a
+                      href="#contact"
                       className="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     >
                       View Details
-                    </button>
+                    </a>
                   </div>
                 </div>
                 {selectedJob && (
